ded2f8e8bacae2d4d9e41cc24bdbd0489c80f819
Allan Muturi <allan.muturi@elige-africa.com>
2024-12-04 16:00:13 +0300

    initial commit

---
+++ PennyTrail/PennyTrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyTrailApp.swift
+//  PennyTrail
+//
+//  Created by Allan on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PennyTrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
