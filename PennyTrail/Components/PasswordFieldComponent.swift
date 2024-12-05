4408933754116fd4564e9b5142255cb1835e3757
Allan Muturi <allan.muturi@elige-africa.com>
2024-12-05 15:19:08 +0300

    feat: add colors extension

---
+++ PennyTrail/Components/PasswordFieldComponent.swift
@@ -0,0 +1,18 @@
+//
+//  PasswordFieldComponent.swift
+//  PennyTrail
+//
+//  Created by Allan on 12/5/24.
+//
+
+import SwiftUI
+
+struct PasswordFieldComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PasswordFieldComponent()
+}
