1f0b6bc9e9b5e3d447978bcda20fc54abb0b0e7d
Allan Muturi <allan.muturi@elige-africa.com>
2024-12-06 21:14:42 +0300

    feat: add summary card component

--- PennyTrail/Components/SummaryCardComponent.swift
+++ PennyTrail/Components/SummaryCardComponent.swift
@@ -8,11 +8,20 @@
 import SwiftUI
 
 struct SummaryCardComponent: View {
+    var cardTitle: String
+    var cardAmount: String
+    var cardColor: Int = 0xFFFFFF
+    var cardTitleColor: Int = 0x696868
+    var CardAmountColor: Int = 0x201F24
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            Text(cardTitle).foregroundStyle(Color(hex: cardTitleColor)).padding(.horizontal).padding(.top).padding(.bottom, 8)
+            Text("$\(cardAmount)").fontWeight(.bold).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundStyle(Color(hex: CardAmountColor)).padding(.horizontal).padding(.bottom, 20)
+        }.frame(maxWidth: .infinity, alignment: .leading).background(Color(hex: cardColor)).cornerRadius(10).shadow(radius: 0)
+            .padding(.horizontal, 20).textFieldStyle(.roundedBorder)
     }
 }
 
 #Preview {
-    SummaryCardComponent()
+    SummaryCardComponent(cardTitle: "Current Balance", cardAmount: "3,814.25",cardColor: 0x000000, cardTitleColor: 0xFFFFFF, CardAmountColor: 0xFFFFFF)
 }
